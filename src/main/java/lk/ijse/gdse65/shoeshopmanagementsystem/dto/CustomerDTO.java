a4c61c636a51f152632da613657fe062df210999
AnuMPrabhath <anushkaprabhath15@gmail.com>
2024-04-25 14:10:24 +0530

    add customerDto class

--- src/main/java/lk/ijse/gdse65/shoeshopmanagementsystem/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/gdse65/shoeshopmanagementsystem/dto/CustomerDTO.java
@@ -1,4 +1,29 @@
 package lk.ijse.gdse65.shoeshopmanagementsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CustomerDTO {
+    private String customerCode;
+    private String customerName;
+    private String gender;
+    private Date joinDate;
+    private String level;
+    private Integer totalPoints;
+    private Date dob;
+    private String addressLine01;
+    private String addressLine02;
+    private String addressLine03;
+    private String addressLine04;
+    private String addressLine05;
+    private String contactNo;
+    private String email;
+    private Timestamp RecentPurchaseDate;
 }
