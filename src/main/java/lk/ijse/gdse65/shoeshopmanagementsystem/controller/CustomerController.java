90a6a0ecc8fba40204aca240625465ed488718b9
AnuMPrabhath <anushkaprabhath15@gmail.com>
2024-05-03 20:55:39 +0530

    commit CustomerController changes

--- src/main/java/lk/ijse/gdse65/shoeshopmanagementsystem/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse65/shoeshopmanagementsystem/controller/CustomerController.java
@@ -1,11 +1,15 @@
 package lk.ijse.gdse65.shoeshopmanagementsystem.controller;
 
 import lk.ijse.gdse65.shoeshopmanagementsystem.dto.CustomerDTO;
+import lk.ijse.gdse65.shoeshopmanagementsystem.exception.NotFoundException;
 import lk.ijse.gdse65.shoeshopmanagementsystem.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -17,45 +21,78 @@ public class CustomerController {
         return "CustomerHealth Test";
     }
 
-    @PostMapping
-    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customer){
-        return customerService.saveCustomer(customer);
-    }
-    /*@ResponseStatus(HttpStatus.CREATED)
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> saveCustomer(@Validated @RequestBody CustomerDTO customerDTO,
-                                               BindingResult bindingResult) {
+    public ResponseEntity<?> saveCustomer(@Validated @RequestBody CustomerDTO customerDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                    body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+        }
+        try {
+            customerService.saveCustomer(customerDTO);
+            return ResponseEntity.ok("Customer Details saved Successfully.");
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("Internal server error | Customer saved Unsuccessfully.\nMore Details\n"+exception);
+        }
+    }
+
+    @GetMapping(value = "/{code}",produces = "application/json")
+    public ResponseEntity<?> getCustomerByCode(@PathVariable ("code") String code){
+        try {
+            return ResponseEntity.ok(customerService.getSelectedCustomer(code));
+        } catch (NotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found.");
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("Internal server error | Customer Details fetched Unsuccessfully.\nMore Reason\n"+exception);
+        }
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<?> getAllCustomers(){
+        try {
+            return ResponseEntity.ok(customerService.getAllCustomers());
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("Internal server error | Customer Details fetched Unsuccessfully.\nMore Reason\n"+exception);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{code}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable ("code") String code){
+        try {
+            customerService.deleteCustomer(code);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Customer Details deleted Successfully.");
+        } catch (NotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found.");
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("Internal server error | Customer Details deleted Unsuccessfully.\nMore Reason\n"+exception);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{code}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateCustomer(@Validated @RequestBody CustomerDTO customerDTO,
+                                                 BindingResult bindingResult,
+                                                 @PathVariable ("code") String code) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).
                     body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
         }
 
         try {
-            customerService.saveCustomer(customerDTO);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Customer Details saved Successfully.");
+            customerService.updateCustomer(code, customerDTO);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Customer Details Updated Successfully.");
+        } catch (NotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found.");
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
-                    body("Internal server error | Customer saved Unsuccessfully.\nMore Details\n"+exception);
+                    body("Internal server error | Customer Details Updated Unsuccessfully.\nMore Reason\n"+exception);
         }
-    }*/
 
-    @DeleteMapping(value = "/{customerCode}")
-    public void deleteCustomer(@PathVariable ("customerCode") String customerCode){
-        customerService.deleteCustomer(customerCode);
-    }
-
-    @GetMapping
-    List<CustomerDTO> getAll(){
-        return customerService.getAllCustomers();
-    }
-
-    @GetMapping(value = "/{id}")
-    CustomerDTO getCustomerById(@PathVariable("id") String id){
-        return customerService.getSelectedCustomer(id);
-    }
-
-    @PutMapping
-    void updateCustomer(@RequestBody CustomerDTO customerDTO){
-        customerService.updateCustomer(customerDTO.getCustomerCode() ,customerDTO);
     }
 }
